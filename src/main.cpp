fa9b93024499d921d5c5bf1815f7b15e19571588
Istvan Mayer <istvan.mayer1999@protonmail.com>
2025-12-31 15:37:02 +0200

    removed everyting because it was windows template. Instead wrote a makefile using a shared library. Did sensible project structure for once.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "raylib.h"
+
+using namespace std;
+
+int main () {
+    const int screen_width = 1280;
+    const int screen_height = 800;
+    InitWindow(screen_width, screen_height, "Pong game");
+    SetTargetFPS(60);
+
+    while(WindowShouldClose() == false) {
+        BeginDrawing();
+
+        DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
+        DrawRectangle(10,screen_height/2 - 60,25,120, WHITE);
+        DrawRectangle(screen_width-35,screen_height/2 - 60,25,120, WHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
