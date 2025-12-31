9939c2b1265e5b88671c9f17472b8eb39f548ae2
Istvan Mayer <istvan.mayer1999@protonmail.com>
2025-12-31 14:53:54 +0200

    initial commit, starting from the template.

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+int main () {
+    const int screen_width = 1280;
+    const int screen_height = 800;
+    InitWindow(screen_width, screen_height, "Pong game");
+    SetTarget(60);
+
+
+    return 0;
+}
